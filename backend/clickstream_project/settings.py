cf914e7209830ca712a50b05c25b74e003d8d80d
singh-deepanshu-578 <singhdeepanshu578@gmail.com>
2026-08-26 04:52:56 +0530

    Fix: configure STATIC_ROOT and whitenoise for Render deployment

--- backend/clickstream_project/settings.py
+++ backend/clickstream_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
